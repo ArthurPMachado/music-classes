811e7b795dc4d8bcd4970939d871d8f1aaee37f3
Arthur Machado <arthur.machado@iclubs.tech>
2023-12-05 17:23:26 -0300

    feat: create auth module and jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { Env } from '@/env'
+import { TokenSchema, tokenSchema } from '@/schemas/token-schema'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService<Env, true>) {
+    const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: Buffer.from(publicKey, 'base64'),
+      algorithm: ['RS256'],
+    })
+  }
+
+  async validate(payload: TokenSchema) {
+    return tokenSchema.parse(payload)
+  }
+}
