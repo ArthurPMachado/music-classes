73ef76e57a28bec6f18c6c234fb9058a34a07837
Arthur Machado <arthur.machado@iclubs.tech>
2023-12-07 17:16:56 -0300

    test: create authenticate and create account controllers e2e tests

---
+++ test/e2e/authenticate-controller.e2e-spec.ts
@@ -0,0 +1,45 @@
+import request from 'supertest'
+import { hash } from 'bcryptjs'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { PrismaService } from '@/prisma/prisma.service'
+import { AppModule } from '@/app.module'
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'Fulano',
+        email: 'fulanoteste@example.com',
+        password: await hash('123456', 10),
+        is_admin: false,
+        has_permission: false,
+      },
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'fulanoteste@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual({
+      access_token: expect.any(String),
+    })
+  })
+})
