6cf85153951cfe487fb29d38d5841db53aa81774
Arthur Machado <arthur.machado@iclubs.tech>
2024-04-08 15:55:49 -0300

    style: rename user metrics file name and class

--- src/domain/application/use-cases/get-users-metrics.ts
+++ src/domain/application/use-cases/get-users-metrics.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { IUsersRepository } from '../repositories/users-repository'
-import { IUsersMetricsUseCaseResponse } from './interfaces/IUsersMetricsUseCaseResponse'
+import { IGetUsersMetricsUseCaseResponse } from './interfaces/IGetUsersMetricsUseCaseResponse'
 
 @Injectable()
 export class GetUsersMetricsUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  async execute(): Promise<IUsersMetricsUseCaseResponse> {
+  async execute(): Promise<IGetUsersMetricsUseCaseResponse> {
     const totalUsers = await this.usersRepository.getTotalUsers()
 
     const totalUsersWithAccess = totalUsers.filter(
---
+++ src/domain/application/use-cases/interfaces/IGetUsersMetricsUseCaseResponse.ts
@@ -0,0 +1,4 @@
+export interface IGetUsersMetricsUseCaseResponse {
+  totalUsers: number
+  totalUsersWithAccess: number
+}
