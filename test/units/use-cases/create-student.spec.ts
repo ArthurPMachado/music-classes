07ed4acd4248d9f3c1a6b00824aa00442c23e7d5
Arthur Machado <arthur.machado@iclubs.tech>
2024-04-18 17:18:50 -0300

    test: create student

---
+++ test/units/use-cases/create-student.spec.ts
@@ -0,0 +1,43 @@
+import { CreateStudentUseCase } from '@/domain/application/use-cases/create-student'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+
+let sut: CreateStudentUseCase
+
+describe('Create Student Use Case', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    fakeHasher = new FakeHasher()
+
+    sut = new CreateStudentUseCase(inMemoryStudentsRepository, fakeHasher)
+  })
+
+  it('Should be able to create a new student', async () => {
+    const result = await sut.execute({
+      name: 'test',
+      email: 'test@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      student: inMemoryStudentsRepository.items[0],
+    })
+  })
+
+  it('Should hash student password upon creation', async () => {
+    const result = await sut.execute({
+      name: 'test',
+      email: 'test@example.com',
+      password: '123456',
+    })
+
+    const hashedPassword = await fakeHasher.hash('123456')
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryStudentsRepository.items[0].password).toEqual(hashedPassword)
+  })
+})
