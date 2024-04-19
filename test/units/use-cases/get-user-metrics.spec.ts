558231232655fe83bbf0fe32815bd9c1636823f9
Arthur Machado <arthur.machado@iclubs.tech>
2024-04-19 18:12:06 -0300

    test: get users metrics

---
+++ test/units/use-cases/get-user-metrics.spec.ts
@@ -0,0 +1,34 @@
+import { FetchStudentsUseCase } from '@/domain/application/use-cases/fetch-students'
+import { GetUsersMetricsUseCase } from '@/domain/application/use-cases/get-users-metrics'
+import { makeStudent } from 'test/factories/make-student'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+
+let sut: GetUsersMetricsUseCase
+
+describe('Get Users Metrics Use Case', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+
+    sut = new GetUsersMetricsUseCase(inMemoryStudentsRepository)
+  })
+
+  it('Should be able to get users metrics', async () => {
+    await inMemoryStudentsRepository.create(makeStudent({
+      hasAccess: true
+    }))
+    await inMemoryStudentsRepository.create(makeStudent({
+      hasAccess: true
+    }))
+    await inMemoryStudentsRepository.create(makeStudent({
+      hasAccess: false
+    }))
+
+    const result = await sut.execute()
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value.totalUsers).toEqual(3)
+    expect(result.value.totalUsersWithAccess).toEqual(2)
+  })
+})
