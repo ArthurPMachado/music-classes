c418e63ddf6085c8c2bd343904913182cd30915d
Arthur Machado <arthur.machado@iclubs.tech>
2024-04-19 19:23:37 -0300

    test: another delete student scenario

--- test/units/use-cases/delete-student.spec.ts
+++ test/units/use-cases/delete-student.spec.ts
@@ -1,4 +1,5 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
 import { DeleteStudentUseCase } from '@/domain/application/use-cases/delete-student'
 import { makeStudent } from 'test/factories/make-student'
 import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
@@ -34,5 +35,13 @@ describe('Delete Student', () => {
 
     expect(inMemoryStudentsRepository.items).toHaveLength(1)
     expect(inMemoryStudentsRepository.items[0].id.toString()).toEqual('to-maintain')
+  })
+
+
+  it('Should not be able to delete a student if it does not exists', async () => {
+    const result = await sut.execute({studentId: 'to-delete'})
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toEqual(new ResourceNotFoundError())
   })
 })
