01771d4d762dae5b02efab064d563f9b7502dc18
Arthur Machado <arthur.machado@iclubs.tech>
2024-05-09 17:29:31 -0300

    refactor: use env service on jwt strategy and on main ts

--- src/infra/auth/jwt.strategy.ts
+++ src/infra/auth/jwt.strategy.ts
@@ -1,14 +1,13 @@
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { Injectable } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
-import { Env } from '@/infra/env/env'
 import { TokenPayload, tokenPayload } from '@/infra/http/schemas/token-schema'
+import { EnvService } from '../env/env.service'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService<Env, true>) {
-    const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+  constructor(env: EnvService) {
+    const publicKey = env.get('JWT_PUBLIC_KEY')
 
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
